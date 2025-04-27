e84878dda0b0a591ba171e0ff8178154a2decb52
satoshi <stomioka1983@gmail.com>
2025-04-27 16:27:45 +0900

    feat: 配送フロー対応のlineController, messageService, flexMessageServiceを更新

--- services/flexMessageService.js
+++ services/flexMessageService.js
@@ -43,7 +43,7 @@ async function sendFlexMessage(replyToken, title, url) {
         type: 'box',
         layout: 'vertical',
         spacing: 'sm',
-        borderWidth: "0.5px",
+        borderWidth: "0.75px",
         contents: [
           {
             type: 'button',
